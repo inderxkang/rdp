804d5d03a134c3891ba652bfbcbbfd9d83613382
inderxkang <160316837+inderxkang@users.noreply.github.com>
2024-11-06 17:45:47 +0530

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -11,7 +11,6 @@ $INTRO = @"
                                                               
 "@
 Write-Host $INTRO
-$PASS = $param1
 Write-Host "Downloading Necessary Resources"
 choco install chrome-remote-desktop-host googlechrome -y -r --no-progress
 Write-Host "Downloading Necessary Resources Done"
