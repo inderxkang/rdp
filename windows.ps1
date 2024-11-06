df67659c38acee3c0d1f2d7dc53ebb54127d8875
inderxkang <160316837+inderxkang@users.noreply.github.com>
2024-11-06 17:43:36 +0530

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,36 +1,3 @@
-﻿##[Ps1 To Exe]
-##
-##Kd3HDZOFADWE8uK1
-##Nc3NCtDXThU=
-##Kd3HFJGZHWLWoLaVvnQnhQ==
-##LM/RF4eFHHGZ7/K1
-##K8rLFtDXTiW5
-##OsHQCZGeTiiZ4tI=
-##OcrLFtDXTiW5
-##LM/BD5WYTiiZ4tI=
-##McvWDJ+OTiiZ4tI=
-##OMvOC56PFnzN8u+Vs1Q=
-##M9jHFoeYB2Hc8u+Vs1Q=
-##PdrWFpmIG2HcofKIo2QX
-##OMfRFJyLFzWE8uK1
-##KsfMAp/KUzWJ0g==
-##OsfOAYaPHGbQvbyVvnQX
-##LNzNAIWJGmPcoKHc7Do3uAuO
-##LNzNAIWJGnvYv7eVvnQX
-##M9zLA5mED3nfu77Q7TV64AuzAgg=
-##NcDWAYKED3nfu77Q7TV64AuzAgg=
-##OMvRB4KDHmHQvbyVvnQX
-##P8HPFJGEFzWE8tI=
-##KNzDAJWHD2fS8u+Vgw==
-##P8HSHYKDCX3N8u+Vgw==
-##LNzLEpGeC3fMu77Ro2k3hQ==
-##L97HB5mLAnfMu77Ro2k3hQ==
-##P8HPCZWEGmaZ7/K1
-##Kc/BRM3KXhU=
-##
-##
-##fd6a9f26a06ea3bc99616d4851b372ba
-$param1=$args[0]
 
 $INTRO = @"
                                                                
@@ -48,12 +15,3 @@ $PASS = $param1
 Write-Host "Downloading Necessary Resources"
 choco install chrome-remote-desktop-host googlechrome -y -r --no-progress
 Write-Host "Downloading Necessary Resources Done"
-Write-Host "Starting Remote Session"
-$PASS -pin=123456
-Write-Host "Remote Session Started Have fun"
-$i = 358
-         do {
-             Write-Host $i
-             Sleep 60
-             $i--
-        } while ($i -gt 0)
